b662eac5428866947272a297d82e4dc3b47f839e
jjenniyun <hjkjj1847@gmail.com>
2023-05-16 22:11:06 +0900

    auth 오류 수정(23.05.16)

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,27 @@
 //import "./App.css";
 
 import React from "react";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, Router } from "react-router-dom";
 
 import LandingPage from "./components/views/LandingPage/LandingPage";
 import LoginPage from "./components/views/LoginPage/LoginPage";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage";
-import Auth from "./hoc/auth";
+//import Auth from "./hoc/auth";
 
 function App() {
-  const LandingPage = Auth(LandingPage,null);
-  const LoginPage = Auth(LoginPage, false);
-  const RegisterPage = Auth(RegisterPage, false);
-
+  //const LandingPage = Auth(LandingPage,null);
+  //const LoginPage = Auth(LoginPage, false);
+  //const RegisterPage = Auth(RegisterPage, false);
   return (
-    <BrowserRouter>
+    <Router>
       <div>
-        {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
         <Routes>
           <Route exact path="/" element={<LandingPage />} />
           <Route exact path="/login" element={<LoginPage />} />
           <Route exact path="/register" element={<RegisterPage />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
